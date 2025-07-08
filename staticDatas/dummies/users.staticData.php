ab68baa125bc333206b765bb6ae018673feafea0
GenesisAdduru <202310334@fit.edu.ph>
2025-07-08 22:24:15 +0800

    feat: added dummy data

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    [
+        'username'   => 'john.smith',
+        'password'   => 'p@ssW0rd1234',
+        'first_name' => 'John',
+        'last_name'  => 'Smith',
+        'role'       => 'designer',
+    ],
+    [
+        'username'   => 'jane.doe',
+        'password'   => 'securePass456!',
+        'first_name' => 'Jane',
+        'last_name'  => 'Doe',
+        'role'       => 'manager',
+    ],
+];
